de5c4247fbe1aa99f20428b2263617cdb7e03fc4
Mário Pereira <mariodmpereira@gmail.com>
2021-10-29 22:52:40 +0100

    Create course files for the sixth course, Basic Algorithm Scripting

---
+++ 00 - Generate Course Files Scripts/06 - Basic Algorithm Scripting.mjs
@@ -0,0 +1,28 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/#basic-algorithm-scripting
+
+import {createExerciseFiles} from './00 - Create Course Files.mjs';
+
+const COURSE = {
+    name: "Basic Algorithm Scripting",
+    number: "06",
+    lessons: [
+        "Convert Celsius to Fahrenheit",
+        "Reverse a String",
+        "Factorialize a Number",
+        "Find the Longest Word in a String",
+        "Return Largest Numbers in Arrays",
+        "Confirm the Ending",
+        "Repeat a String Repeat a String",
+        "Truncate a String",
+        "Finders Keepers",
+        "Boo who",
+        "Title Case a Sentence",
+        "Slice and Splice",
+        "Falsy Bouncer",
+        "Where do I Belong",
+        "Mutations",
+        "Chunky Monkey"
+    ]
+}
+
+createExerciseFiles(COURSE);
