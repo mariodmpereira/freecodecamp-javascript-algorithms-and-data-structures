591485e42fc0876b071169ad8ae8baa1c7a465c6
mariodmpereira <mariodmpereira@gmail.com>
2021-11-08 23:16:35 +0000

    Create course files for the last certification section, JavaScript Algorithms and Data Structures Projects

---
+++ resources/courses/10-javascript-algorithms-and-data-structures-projects.mjs
@@ -0,0 +1,17 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/#javascript-algorithms-and-data-structures-projects
+
+import {createExerciseFiles} from '../create-course-files.mjs';
+
+const COURSE = {
+    name: "JavaScript Algorithms and Data Structures Projects",
+    number: "10",
+    lessons: [
+        "Palindrome Checker",
+        "Roman Numeral Converter",
+        "Caesars Cipher",
+        "Telephone Number Validator",
+        "Cash Register"
+    ]
+}
+
+createExerciseFiles(COURSE);
